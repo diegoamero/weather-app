53aefb9a251af38167078ef3c24a9e1be8c21e2f
Diego Mejia <diegoamejia.10@gmail.com>
2022-09-20 23:59:02 -0400

    Add Day and Time

---
+++ src/components/TimeInfo.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './TimeInfo.css'
+
+function TimeInfo({localTime}) {
+    let date = new Date(localTime),
+        dayName = '',
+        day = date.getDay(),
+        hours = date.getHours(),
+        minutes = date.getMinutes()
+
+        switch (day) {
+            case 0:
+                dayName = 'Sunday'
+                break
+            case 1:
+                dayName = 'Monday'
+                break
+            case 2:
+                dayName = 'Tuesday'
+                break
+            case 3:
+                dayName = 'Wednesday'
+                break
+            case 4:
+                dayName = 'Thursday'
+                break
+            case 5:
+                dayName = 'Friday'
+                break
+            case 6:
+                dayName = 'Saturday'
+                break
+
+        }
+
+    return <div className='TimeInfo'>
+        <div className='TimeInfo--day'>{dayName}</div>
+        <div className='TimeInfo--time'>{hours}:{minutes}</div>
+    </div>
+}
+
+export default TimeInfo
